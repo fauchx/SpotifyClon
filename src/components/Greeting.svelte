c9f41f09c305dd5bc782c9907307d483255c0110
fauchx <fabian.urrutia@correounivalle.edu.co>
2024-06-11 01:01:09 -0500

    Audio

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    const currenTime = new Date()
+    const currentHour = currenTime.getHours()
+    let greeting = ""
+    if (currentHour < 12){
+        greeting = "Buenos dias"
+    } else if (currentHour < 18){
+        greeting = "Buenas tardes"
+    } else{
+        greeting = "Buenas noches"
+    }
+</script>
+<h1 class="text-3xl font-bold">{greeting}</h1>
